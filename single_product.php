a2864b41216faaabbcb3b2ac6b7dbe366e1a9755
curtismchale <curtis@curtismchale.ca>
2011-02-23 13:50:55 -0800

    abstracted the buy button

--- single_product.php
+++ single_product.php
@@ -144,24 +144,7 @@ $image_height = get_option('single_view_image_height');
 
 
 					<!-- END OF QUANTITY OPTION -->
-					<?php if((get_option('hide_addtocart_button') == 0) && (get_option('addtocart_or_buynow') !='1')) : ?>
-						<?php if(wpsc_product_has_stock()) : ?>
-							<?php if(wpsc_product_external_link(wpsc_the_product_id()) != '') : ?>
-										<?php	$action =  wpsc_product_external_link(wpsc_the_product_id()); ?>
-										<input class="wpsc_buy_button" type='button' value='<?php echo __('Buy Now', 'wpsc'); ?>' onclick='gotoexternallink("<?php echo $action; ?>")'>
-										<?php else: ?>
-									<input type="submit" value="<?php echo __('Add To Cart', 'wpsc'); ?>" name="Buy" class="wpsc_buy_button" id="product_<?php echo wpsc_the_product_id(); ?>_submit_button"/>
-										<?php endif; ?>
-
-							<div class='wpsc_loading_animation'>
-								<img title="Loading" alt="Loading" src="<?php echo WPSC_URL ;?>/images/indicator.gif" class="loadingimage" />
-								<?php echo __('Updating cart...', 'wpsc'); ?>
-							</div>
-
-						<?php else : ?>
-							<p class='soldout'><?php echo __('This product has sold out.', 'wpsc'); ?></p>
-						<?php endif ; ?>
-					<?php endif ; ?>
+                        <?php include( 'assets/includes/buy-button.php' ); ?>
 					</form>
 
 					<?php if((get_option('hide_addtocart_button') == 0) && (get_option('addtocart_or_buynow')=='1')) : ?>
@@ -197,6 +180,5 @@ $image_height = get_option('single_view_image_height');
 			echo fancy_notifications();
 		}
 		?>
-
 
 </div>
